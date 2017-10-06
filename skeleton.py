01ba4318a98c32d731f44bfb08327412d8751c83
c14399846@mydit.ie <c14399846@mydit.ie>
2017-10-06 09:32:37 +0100

    Added some Gaussian blur to help clean up the image.

--- skeleton.py
+++ skeleton.py
@@ -10,7 +10,20 @@ size = np.size(image)
 skeleton = np.zeros(image.shape, np.uint8)
 
 
-image = cv2.adaptiveThreshold(image, maxValue = 255, adaptiveMethod = cv2.ADAPTIVE_THRESH_GAUSSIAN_C, 
+kernel = np.ones((5,5),np.float32)/25
+
+blurGaus = cv2.GaussianBlur(image,(5,5),0)
+
+# TESTING: does de-noising, loses some data
+#den = cv2.fastNlMeansDenoising(blurgaus,None,10,7,21)
+#dstden = cv2.filter2D(den,-1,kernel)
+#thden = cv2.adaptiveThreshold(dstden,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY,15,5)
+
+
+
+distGaus = cv2.filter2D(blurGaus,-1,kernel)
+
+image = cv2.adaptiveThreshold(distGaus, maxValue = 255, adaptiveMethod = cv2.ADAPTIVE_THRESH_GAUSSIAN_C, 
 	thresholdType = cv2.THRESH_BINARY, blockSize = 15, C = 5)
 
 # algorithm eroded and dilated the white space in between each line(?) of the fingerprint
@@ -19,7 +32,7 @@ image = cv2.bitwise_not(image)
 
 # 3,3 works the best apparently
 element = cv2.getStructuringElement(cv2.MORPH_CROSS, (3, 3))
-s
+
 done = False
 
 while( not done):
@@ -27,6 +40,11 @@ while( not done):
 	# this does magical things
 	eroded = cv2.erode(image, element)
 	temp = cv2.dilate(eroded, element)
+	
+	#eroded = cv2.dilate(image, element)
+	#temp = cv2.erode(eroded, element)
+	
+	
 	temp = cv2.subtract(image, temp)
 	skeleton = cv2.bitwise_or(skeleton, temp)
 	image = eroded.copy()
@@ -37,9 +55,10 @@ while( not done):
 
 		done = True
 
-# skeleton_inverted = cv2.bitwise_not(skeleton)
+# inverts the colour
+skeleton_inverted = cv2.bitwise_not(skeleton)
 cv2.imshow("original", original)
-cv2.imshow("skeleton", skeleton)
+cv2.imshow("skeleton", skeleton_inverted)
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
